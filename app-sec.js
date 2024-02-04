cc4e4d846a83562b6bdb32c8248955b5ff449811
kaushal <kaushal141992@gmail.com>
2024-02-03 22:10:53 -0600

    updated the code

--- app-sec.js
+++ app-sec.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const js = require('js-yaml');
 module.exports = async ({github, context, core}) => {
 try {
   // `who-to-greet` input defined in action metadata file
   //const nameToGreet = core.getInput('who-to-greet');
   //const veracode = core.getInput('veracode-data');
-  core.setOutput("veracode", sampleapp);
+  core.setOutput("veracode", "sampleapp");
 } catch (error) {
   core.setFailed(error.message);
 }
