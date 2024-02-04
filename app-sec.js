78dbdc5819bfb5476bdc90a1200bd1a87e6614ff
kaushal <kaushal141992@gmail.com>
2024-02-03 22:03:29 -0600

    My first action is ready

--- app-sec.js
+++ app-sec.js
@@ -5,7 +5,6 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const veracode = core.getInput('veracode-data');
-  core.setOutput("time", time);
   core.setOutput("veracode", veracode);
 } catch (error) {
   core.setFailed(error.message);
